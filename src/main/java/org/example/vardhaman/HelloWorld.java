75057da353bf4c787018a4ee52f0b46694eecf8c
saikiranreddy <saikiranreddysingasani7075@gmail.com>
2024-10-25 15:58:50 +0530

    created hello world

---
+++ src/main/java/org/example/vardhaman/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.example.vardhaman;
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for a ");
+        int a = sc.nextInt();
+        System.out.println("The Value of a is : " + a );
+
+
+    }
+}
